5e71930809bf5e48face6fa05b883e59e17cb81d
Varfolomeev <2004_2@tut.by>
2024-06-01 21:14:17 +0300

    Add check of undefined

--- profile/profile.js
+++ profile/profile.js
@@ -1,40 +1,12 @@
 /*
-let user = getUser()
-
-if(localStorage.getItem("lang") == "ru"){
-    if(user["role"] == "admin")[
-        document.querySelector(".login_label").textContent += "администратор"
-    ]
-    else{
-        document.querySelector(".login_label").textContent += "пользователь"
-    }
-}
-else{
-    document.querySelector(".login_label").textContent += user["role"]
-}
-
-document.querySelector("#fname").textContent = user["firstName"]
-document.querySelector("#mname").textContent = user["middleName"]
-document.querySelector("#lname").textContent = user["lastName"]
-document.querySelector("#email").textContent += user["email"]
-document.querySelector("#phoneNum").textContent += user["phoneNum"]
-document.querySelector("#birthDate").textContent += user["birthDate"]
-
-document.querySelector(".sign_out").addEventListener("click", () => {
-    logout()
-    window.location.replace("../index.html")
-})
-*/
 var usersArray = JSON.parse(localStorage.getItem("usersData"));
 function getUser(){
     console.log(usersArray)
     return usersArray[localStorage.getItem('currUserId')];
 }
 
-// Retrieve user information
 let user = getUser();
 
-// Update login label based on user role and language
 if (localStorage.getItem("language") == "ru") {
     if (user["role"] == "admin") {
         document.querySelector(".login_label").textContent += "администратор";
@@ -45,10 +17,50 @@ if (localStorage.getItem("language") == "ru") {
     document.querySelector(".login_label").textContent += user["role"];
 }
 
-// Display user information in the form
 document.querySelector("#fname").textContent = user["name"];
 document.querySelector("#mname").textContent = user["surname"];
 document.querySelector("#lname").textContent = user["patronymic"];
 document.querySelector("#email").textContent += user["eMail"];
 document.querySelector("#phoneNum").textContent += user["phone"];
 document.querySelector("#birthDate").textContent += user["birthDate"];
+*/
+
+var usersArray = JSON.parse(localStorage.getItem("usersData"));
+
+function getUser() {
+  console.log(usersArray);
+  return usersArray[localStorage.getItem("currUserId")];
+}
+
+let user = getUser();
+
+if (localStorage.getItem("language") == "ru") {
+  if (user["role"] == "admin") {
+    document.querySelector(".login_label").textContent += "администратор";
+  } else if (user["role"] != undefined) { // Проверка на undefined
+    document.querySelector(".login_label").textContent += "пользователь";
+  }
+} else {
+  if (user["role"] != undefined) { // Проверка на undefined
+    document.querySelector(".login_label").textContent += user["role"];
+  }
+}
+
+if (user["name"] != undefined) { // Проверка на undefined
+  document.querySelector("#fname").textContent = user["name"];
+}
+if (user["surname"] != undefined) { // Проверка на undefined
+  document.querySelector("#mname").textContent = user["surname"];
+}
+if (user["patronymic"] != undefined) { // Проверка на undefined
+  document.querySelector("#lname").textContent = user["patronymic"];
+}
+if (user["eMail"] != undefined) { // Проверка на undefined
+  document.querySelector("#email").textContent += user["eMail"];
+}
+if (user["phone"] != undefined) { // Проверка на undefined
+  document.querySelector("#phoneNum").textContent += user["phone"];
+}
+if (user["birthDate"] != undefined) { // Проверка на undefined
+  document.querySelector("#birthDate").textContent += user["birthDate"];
+}
